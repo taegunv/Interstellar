1aa438756af09a09e08f0dbe1f0a2125023585d6
taegunv <vickyliu4321@gmail.com>
2024-09-01 13:51:03 -0400

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
   challenge: true, // Set to true if you want to enable password protection.
   blocked: {
     // Normal users can ignore this option. This is for the owner of the website.
-    "gointerstellar.app": "",
+    "gointerstellar.app": "config=true npm start",
     localhost: "",
   },
   users: {
